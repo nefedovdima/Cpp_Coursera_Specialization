6bbb5431528249b75861194ce3e1f9cf7c4d0d64
Нефедов Дмитрий <nefedovdl@mail.ru>
2023-07-15 16:47:36 +0300

    Solved problems 5-8.

--- White_1/Week_1/main.cpp
+++ White_1/Week_1/main.cpp
@@ -2,7 +2,7 @@ using namespace std;
 #include <iostream>
 #include <string>
 #include <cmath>
-
+#include <vector>
 
 // 1.Минимальная строка
 /*
@@ -74,3 +74,74 @@ int main() {
 }
 */
 
+// 5. Четные числа
+/*
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a % 2 == 1)
+        a++;
+    // a становится первым четным >= a
+    while (a <= b) {
+        cout << a << " ";
+        a += 2;
+    }
+    return 0;
+}
+*/
+
+
+// 6. Второе вхождение
+/*
+int main() {
+    string s;
+    cin >> s;
+    int meet_f = -2;
+    for (int i=0; i < s.size(); ++i) {
+        if (s[i] == 'f') {
+            if (meet_f == -2)   // если f встретилась первый раз
+                meet_f = -1;    //ставим f в -1
+            else {              // если f встретилась второй раз
+                meet_f = i;     //записываем в f индекс второго вхождения
+                break;          // и выходим
+            }
+        }
+    }
+    cout << meet_f;
+}
+ */
+
+// 7. НОД
+//Работает только на малых a и b
+/*
+int main() {
+    int a, b;
+    cin >> a >> b;
+    while (b != 0) {
+        int tmp;
+        // a, b = b, a % b
+        tmp = a;
+        a = b;
+        b = tmp % b;
+    }
+    cout << a;
+    return 0;
+}
+ */
+
+// 8. Двоичные числа
+/*
+int main() {
+    int n;
+    cin >> n;
+    vector<int> bits;
+    while (n != 0) {
+        bits.push_back(n % 2);
+        n /= 2;
+    }
+    for (int i=0; i< bits.size(); ++i) {
+        cout << bits[bits.size()-1-i];
+    }
+    return 0;
+}
+ */

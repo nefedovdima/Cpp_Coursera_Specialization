2650892044bdf010e471a685ce5134ee75843c2c
Нефедов Дмитрий <nefedovdl@mail.ru>
2023-07-29 17:13:31 +0300

    Проыедена декомпозиция решения задачи 'Синонимы'

--- Yellow_2/Week_2/synonyms.cpp
+++ Yellow_2/Week_2/synonyms.cpp
@@ -8,6 +8,24 @@
 #include <set>
 using namespace std;
 
+
+void AddSynonyms(map<string, set<string>>& synonyms,
+                 const string& first_word,
+                 const string& second_word) {
+    synonyms[second_word].insert(first_word);
+    synonyms[first_word].insert(second_word);
+}
+
+size_t GetSynonymsCount(map<string, set<string>>& synonyms, const string& word) {
+    return synonyms[word].size();
+}
+
+bool AreSynonyms(map<string, set<string>>& synonyms,
+                     const string& first_word,
+                     const string& second_word) {
+    return synonyms[first_word].count(second_word) == 1;
+}
+
 int main() {
     int q;
     cin >> q;
@@ -21,16 +39,15 @@ int main() {
         if (operation_code == "ADD") {
             string first_word, second_word;
             cin >> first_word >> second_word;
-            synonyms[second_word].insert(first_word);
-            synonyms[first_word].insert(second_word);
+            AddSynonyms(synonyms, first_word, second_word);
         } else if (operation_code == "COUNT") {
             string word;
             cin >> word;
-            cout << synonyms[word].size() << endl;
+            cout << GetSynonymsCount(synonyms, word) << endl;
         } else if (operation_code == "CHECK") {
             string first_word, second_word;
             cin >> first_word >> second_word;
-            if (synonyms[first_word].count(second_word) == 1)
+            if (AreSynonyms(synonyms, first_word, second_word))
                 cout << "YES" << "\n";
             else
                 cout << "NO" << "\n";

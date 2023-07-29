fa33c161622a870ce9231b47423e23bdf14651e3
Нефедов Дмитрий <nefedovdl@mail.ru>
2023-07-29 15:44:45 +0300

    Решена задача ссылка GetRefStrict

--- Yellow_2/Week_1/get_ref_strict.cpp
+++ Yellow_2/Week_1/get_ref_strict.cpp
@@ -1,3 +1,25 @@
-//
-// Created by User on 29.07.2023.
-//
+#include <iostream>
+#include <map>
+#include <vector>
+#include <tuple>
+#include <utility>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+
+template <typename Key, typename Value>
+Value& GetRefStrict(map<Key, Value>& m, const Key& key) {
+    if (m.count(key) == 0) {
+        throw runtime_error("Ключа " + to_string(key) + "нет в словаре");
+    }
+    return m.at(key);
+}
+
+
+int main() {
+    map<int, string> m = {{0, "value"}};
+    string& item = GetRefStrict(m, 0);
+    item = "newvalue";
+    cout << m[0] << endl;
+    return 0;
+}
